d34bae1649191ffc0012f30d5ce4b8aebe6a3213
weichangk <18276743761@163.com>
2022-06-25 16:23:38 +0800

    add

--- MiniShop.Backend.Api/Migrations/20220625082034_UpdateVip202206251620.Designer.cs
+++ MiniShop.Backend.Api/Migrations/20220625082034_UpdateVip202206251620.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MiniShop.Backend.Model.Code;
 
 namespace MiniShop.Backend.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220625082034_UpdateVip202206251620")]
+    partial class UpdateVip202206251620
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
